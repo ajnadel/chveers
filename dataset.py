1ce816b5efb8df0247b8f3ac5593c26d2986bf7c
AJ Nadel <aj@ajnadel.com>
2022-03-12 20:27:06 -0800

    initial commit

---
+++ dataset.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import os
+from torch.utils.data import Dataset
+from transformers import GPT2Tokenizer
+import torch
+
+WANDB_PROJECT_DATASET = 'datasets'
+
+STRAT_WITHHOLD_LAST_N = 0
+STRAT_WITHHOLD_LAST_PCT = 1
+
+def datasets_from_csv(filename, *, strategy = 1, withhold_param = 0.5, dev = 30, random_dev=False):
+  """Read in and preprocess CSV data from a bytestring. Returns (train_set, dev_set)"""
+  parsed_csv = pd.read_csv(filename)
+  print(f"Processing {len(parsed_csv)} examples...")
+
+  all_examples = parsed_csv[["Body"]]
+
+  if not random_dev:
+    dev_set = all_examples.iloc[:dev]
+    train_set = all_examples.iloc[dev:]
+  else:
+    dev_set = train_set.sample(n = dev)
+    train_set = all_examples.loc[~all_examples.index.isin(dev_set.index)]
+
+  train_set = train_set.copy().reset_index(drop=True)
+  dev_set = dev_set.copy().reset_index(drop=True)
+
+  if strategy == STRAT_WITHHOLD_LAST_N:
+    dev_set['Body_prefix'] = dev_set['Body'].str.split().str[:-withhold_param].apply(' '.join)
+    dev_set['Body_suffix_gold'] = dev_set['Body'].str.split().str[-withhold_param:].apply(' '.join)
+  elif strategy == STRAT_WITHHOLD_LAST_PCT:
+    dev_set['Body_prefix'] = dev_set['Body'].str.split().apply(lambda words: ' '.join(words[:-int(len(words) * withhold_param)]))
+    dev_set['Body_suffix_gold'] = dev_set['Body'].str.split().apply(lambda words: ' '.join(words[-int(len(words) * withhold_param):]))
+  else:
+    raise ValueError('Invalid `strategy` parameter (expected int enum option)')
+
+  return train_set, dev_set
+
+
+class EmailBodyDataset(Dataset):
+  def __init__(self, df, gpt2_type="gpt2", max_length=128, debug=False):
+    self.tokenizer = GPT2Tokenizer.from_pretrained(gpt2_type)
+    
+    self.data = []
+    for email_body in df["Body"]:
+      email_body = email_body.rstrip()
+      self.data.append(
+        torch.tensor(
+          self.tokenizer.encode(
+            f"{self.tokenizer.bos_token}{email_body}{self.tokenizer.eos_token}"
+          )
+        )
+      )
+      if debug:
+        print(f"{self.start_token}{email_body}{self.end_token}\n")
+        print(self.data[-1])
+        print(len(self.data[-1]))
+
+  def __len__(self):
+    return len(self.data)
+
+  def __getitem__(self, idx):
+      return self.data[idx]
+
+if __name__ == '__main__':
+  import argparse
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('dataset_name',
+    help="Name token for the dataset.")
+  parser.add_argument('csv_filepath',
+    help="Path in the local filesystem to a properly-formatted CSV file.")
+  strategy_group = parser.add_mutually_exclusive_group(required=True)
+  strategy_group.add_argument('-p', '--withhold-pct', type=float,
+    help="Withold the last x% of each example for training.")
+  strategy_group.add_argument('-n', '--withhold-n', type=int,
+    help="Withold the last n words of each example for training.")
+  parser.add_argument('-o', '--outfile-prefix',
+    help="Path prefix for train and dev dataset output csvs")
+  
+  parser.add_argument('-d', '--num-dev-examples', type=int, required=True,
+    help="The number of examples to reserve for dev (validation) set.")
+
+  parser.add_argument('-u', '--upload', dest="wandb", action="store_true",
+    help="Upload resulting datasets to WandB.")
+
+  args = parser.parse_args()
+
+  # start wandb
+  if args.wandb:
+    import wandb
+    run = wandb.init(project=WANDB_PROJECT_DATASETS, entity="chveers", job_type='preprocess_data')
+
+  if args.withhold_n is not None:
+    strategy = STRAT_WITHHOLD_LAST_N
+    withhold_param = args.withhold_n
+  else:
+    strategy = STRAT_WITHHOLD_LAST_PCT
+    withhold_param = args.withhold_pct
+  print(f"{args}, p=")
+
+  train_set, dev_set = datasets_from_csv(args.csv_filepath,
+                                        strategy=strategy,
+                                        withhold_param=withhold_param,
+                                        dev=args.num_dev_examples)
+
+  out_prefix = args.outfile_prefix or args.dataset_name
+  
+  train_out = f"{out_prefix}_train.csv"
+  dev_out = f"{out_prefix}_dev.csv"
+  print(f"Writing training set to '{train_out}'... ", end="")
+  train_set.to_csv(train_out, index=False)
+  print('done.')
+  print(f"Writing dev set to '{dev_out}'... ", end="")
+  dev_set.to_csv(dev_out, index=False)
+  print('done.')
+
+
+  if args.wandb:
+    data_artifact = wandb.Artifact(args.dataset_name, type="proprocessed_data")
+    data_artifact.add_file(train_out)
+    data_artifact.add_file(dev_out)
+    run.log_artifact(data_artifact)
---
+++ prefix_tuning.py
@@ -0,0 +1,136 @@
+# from transformers import Trainer
+import torch
+from transformers import PreTrainedModel, GPT2PreTrainedModel, GPT2Tokenizer
+from torch import nn
+
+
+class PrefixTuning(GPT2PreTrainedModel):
+    """Classification Head for  transformer encoders"""
+    def __init__(self, config, model_gpt2, #optim_prefix=False, 
+        preseqlen=5, # Number of tokens in the prefix
+        deep_param=False
+        ):
+        super().__init__(config)
+        print('under the PrefixTuning model')
+
+        # 1: Init some hyperparameters
+        self.match_n_layer = config.n_layer                 # Number of layers
+        self.match_n_head = config.n_head                   # Number of attention heads
+        self.match_n_embd = config.n_embd // config.n_head  # Embedding dimension PER HEAD
+        self.n_embd = config.n_embd                         # Total embedding dimension
+
+        # 2: Take in some other hyperparams
+
+        if hasattr(config, 'preseqlen'):
+            self.preseqlen = config.preseqlen
+        else:
+            self.preseqlen = preseqlen
+
+        self.tuning_mode = 'prefixtune'
+
+        self.task_mode = 'generate'
+
+        if hasattr(config, 'prefix_dropout'):
+            self.prefix_dropout = config.prefix_dropout
+        else:
+            self.prefix_dropout = 0.0
+
+        self.init_random = False
+
+        self.mid_dim = 512
+
+
+        self.mode_para = 0
+        print('PrefixTuning')
+        print('preseqlen is {}, optimizing the prefix directly'.format(self.preseqlen))
+    
+        low_data_init = 0
+        print('[Full prefix-tuning Setting :) ]')
+        self.input_tokens = torch.arange(self.preseqlen).long()
+        self.wte = nn.Embedding(self.preseqlen, config.n_embd)
+        self.control_trans = nn.Sequential(
+            nn.Linear(config.n_embd, self.mid_dim),
+            nn.Tanh(),
+            nn.Linear(self.mid_dim, config.n_layer * 2 * config.n_embd))
+
+        self.dropout = nn.Dropout(self.prefix_dropout)
+        
+
+    # I think this is the primary one.
+    def get_prompt(self, gpt2=None, bsz=None):
+        # 1: Get input tokens indices (the prefix/continuous prompt) X = [1,2,3,4,5]
+        # 2: Add batch size as a first dimension (unsqueeze & expand)
+        # 3: Put on cuda/cpu depending on self.device setting
+        input_tokens = self.input_tokens.unsqueeze(0).expand(bsz, -1).to(self.device)
+        # 4: Map input tokens through word_embeddings
+        # this is the part that actually learns the embeddings
+        temp_control = self.wte(input_tokens) # X = [1,2,3,4,5] --> [] 
+        # do some stuff. feed forward to mid_dim, then apply non-linearity, then back to full
+        past_key_values = self.control_trans(temp_control) #bsz, seqlen, layer*emb
+
+        # 6: Get the shape, resize it to prepare for attention!! A-TENNNN-SHUN!
+        bsz, seqlen, _ = past_key_values.shape
+        past_key_values = past_key_values.view(bsz, seqlen, self.match_n_layer * 2, self.match_n_head,
+                                               self.match_n_embd)
+        # 7: Apply dropout (word to Elizabeth Holmes).
+        past_key_values = self.dropout(past_key_values)
+        past_key_values = past_key_values.permute([2, 0, 3, 1, 4]).split(2) # wht the fuck?
+        return past_key_values
+
+
+
+    def forward(self,
+        input_ids=None,
+        weights=None,
+        emb_match=None,
+        past_key_values=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        encoder_hidden_states=None,
+        encoder_attention_mask=None,
+        labels=None,
+        use_cache=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        gpt2_model=None,
+        src=None,
+        tgt=None,
+        src_attn=None,
+        tgt_attn=None,
+        **kwargs,
+        ):
+
+        #{"input_ids": batch, "labels": labels, 'src_attn': src_attn, 'tgt_attn':tgt_attn, 'src':src}
+
+        bsz = input_ids.shape[0]
+
+        # Calculate the prefix (this is like a `forward` method for the prompt module)
+        past_key_values_prompt = self.get_prompt(gpt2=gpt2_model, bsz=bsz)
+
+        # Make sure that past_key_values is free for our use (to add the prefix)
+        if past_key_values is not None:
+            assert False, "Attention, use past_key_values for other things"
+        else:
+            past_key_values = past_key_values_prompt
+
+        # Make sure our gpt model is available
+        if gpt2_model is None:
+            assert False, "Didn't specify gpt2 model"
+
+        if self.mode_para == 2 and src_attn is not None and tgt_attn is not None:
+            attention_mask = torch.cat([src_attn, tgt_attn], dim=1)
+
+        output = gpt2_model(input_ids=input_ids, #control_code=None,
+                            weights=weights, emb_match=emb_match,
+                            past_key_values=past_key_values, attention_mask=attention_mask,
+                            token_type_ids=token_type_ids, position_ids=position_ids,
+                           head_mask=head_mask, inputs_embeds=inputs_embeds, encoder_hidden_states=encoder_hidden_states,
+                           encoder_attention_mask=encoder_attention_mask, labels=labels, use_cache=use_cache,
+                           output_attentions=output_attentions, output_hidden_states=output_hidden_states,
+                           return_dict=return_dict, **kwargs)
+
+        return output
